739a6b0942fd5481cb501e600c560cee3238c93a
김태영 <gimtaeyeong@gimtaeyeong-ui-MacBook-Pro.local>
2019-01-15 15:56:24 +0900

    add CORS in app.js

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
+var cors = require('cors');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// CORS
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,12 +23,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404).send({ error: 'Not found' })
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
