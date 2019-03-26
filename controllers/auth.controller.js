6554376baf21e7a8338558d5a1817a2b730c811d
태영 <fnkillur@naver.com>
2019-03-26 13:33:15 +0900

    자동정렬로 인한 띄어쓰기 변경

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,7 +6,7 @@ const ISSUER = 'api.reservacation.com';
 const jwtConfig = require('../config/jwt');
 
 let makeToken = (id) => {
-    return jwt.sign({ id }, jwtConfig.secret, {
+    return jwt.sign({id}, jwtConfig.secret, {
         issuer: ISSUER,
         expiresIn: jwtConfig.tokenLife,
         subject: 'userInfo'
@@ -15,7 +15,7 @@ let makeToken = (id) => {
 
 module.exports = {
     getToken(req, res) {
-        const { email, password } = req.body;
+        const {email, password} = req.body;
 
         return Users.findOne({
             where: {
@@ -23,15 +23,15 @@ module.exports = {
             }
         }).then(user => {
             if (!user) {
-                return res.status(404).send({ message: '등록된 이메일이 아닙니다.' });
+                return res.status(404).send({message: '등록된 이메일이 아닙니다.'});
             }
             let inputPassword = crypto.createHash('sha512').update(password + user.salt).digest('base64');
             if (user.password === inputPassword) {
                 let token = makeToken(user.id);
 
-                return res.status(200).send({ token });
+                return res.status(200).send({token});
             } else {
-                return res.status(403).send({ message: '비밀번호가 일치하지 않습니다.' })
+                return res.status(403).send({message: '비밀번호가 일치하지 않습니다.'})
             }
         }).catch(error => res.status(400).send(error));
     },
@@ -40,9 +40,9 @@ module.exports = {
         try {
             let originToken = req.headers.authorization.split(' ')[1];
             let decodedToken = jwt.verify(originToken, jwtConfig.secret);
-            return res.status(200).send({ token: makeToken(decodedToken.id) });
+            return res.status(200).send({token: makeToken(decodedToken.id)});
         } catch (error) {
-            return res.status(401).send({ message: '토큰이 유효하지 않습니다.' });
+            return res.status(401).send({message: '토큰이 유효하지 않습니다.'});
         }
     },
 };
